fbe147b002da84c79da0ebc676fb9b447ec3f7f0
bhupinder759 <bhupinderkarnawal@gmail.com>
2025-07-24 19:47:45 +0530

    changes

--- server/controllers/auth/authController.js
+++ server/controllers/auth/authController.js
@@ -72,7 +72,7 @@ const loginUser = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: false,
+        secure: true,
       })
       .json({
         success: true,
